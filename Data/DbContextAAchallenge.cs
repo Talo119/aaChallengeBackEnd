0fefa1c349aa6415610adbd4bf673a5c4ddd3ce5
Carlos Motiño <carlos.motino911@gmail.com>
2020-10-17 16:21:00 -0600

    Add class DbContext to Data project.

---
+++ Data/DbContextAAchallenge.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data
+{
+    class DbContextAAchallenge : DbContext
+    {
+
+        public DbContextAAchallenge(DbContextOptions<DbContextAAchallenge> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
