6a02cb317d93fd1500af13f14bfd9ac8db25d5d1
Carlos Motiño <carlos.motino911@gmail.com>
2020-10-20 22:26:19 -0600

    NewtonSoftJason is added.

--- AAchallenge.Web/Startup.cs
+++ AAchallenge.Web/Startup.cs
@@ -27,7 +27,7 @@ namespace AAchallenge.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson();
             //services.AddSwaggerGen();            
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
             services.AddDbContext<DbContextAAchallenge>(options =>
