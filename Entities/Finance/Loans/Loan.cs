9290e5cdbde05be8bd10d5b042e159a95dbcc540
Carlos Motiño <carlos.motino911@gmail.com>
2020-10-21 21:31:45 -0600

    Loan entitie is added.

---
+++ Entities/Finance/Loans/Loan.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using Entities.Clients;
+
+namespace Entities.Finance.Loans
+{
+    public class Loan
+    {
+        public int idloan { get; set; }
+        [Required]
+        public int idclient { get; set; }
+        [Required]
+        public decimal capital { get; set; }
+        [Required]
+        public decimal interest_rate { get; set; }
+        [Required]
+        public int period { get; set; }
+        [Required]
+        public decimal interest_to_pay { get; set; }
+        [Required]
+        public decimal amount_to_finance { get; set; }
+        [Required]
+        public decimal fee { get; set; }
+
+        public Client client { get; set; }
+
+    }
+}
