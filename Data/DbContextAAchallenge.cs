2818a7a42944aef5c2403bd16f02ec0fa1c51f0e
Carlos Motiño <carlos.motino911@gmail.com>
2020-10-25 17:35:54 -0600

    RoleMap is added to DbContext.

--- Data/DbContextAAchallenge.cs
+++ Data/DbContextAAchallenge.cs
@@ -1,9 +1,11 @@
 ﻿using Data.Mapping.Clients;
 using Data.Mapping.Finance.Loans;
 using Data.Mapping.Finance.Payments;
+using Data.Mapping.Users;
 using Entities.Clients;
 using Entities.Finance.Loans;
 using Entities.Finance.Payments;
+using Entities.Users;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -16,6 +18,7 @@ namespace Data
         public DbSet<Client> Clients { get; set; }
         public DbSet<Loan> Loans { get; set; }
         public DbSet<Payment> Payments { get; set; }
+        public DbSet<Role> Roles { get; set; }
 
         public DbContextAAchallenge(DbContextOptions<DbContextAAchallenge> options) : base(options)
         {
@@ -38,6 +41,7 @@ namespace Data
             modelBuilder.ApplyConfiguration(new ClientMap());
             modelBuilder.ApplyConfiguration(new LoanMap());
             modelBuilder.ApplyConfiguration(new PaymentMap());
+            modelBuilder.ApplyConfiguration(new RoleMap());
         }
 
     }
