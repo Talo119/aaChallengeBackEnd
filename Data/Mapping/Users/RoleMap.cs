831f5010fe88ace4c3bafa9d5504cc873fc4f1f4
Carlos Motiño <carlos.motino911@gmail.com>
2020-10-25 17:31:08 -0600

    Role  map is added.

---
+++ Data/Mapping/Users/RoleMap.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Users;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.Mapping.Users
+{
+    public class RoleMap : IEntityTypeConfiguration<Role>
+    {
+        public void Configure(EntityTypeBuilder<Role> builder)
+        {
+            builder.ToTable("role")
+                .HasKey(r => r.idrole);
+        }
+    }
+}
